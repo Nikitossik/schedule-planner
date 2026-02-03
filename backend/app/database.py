3affaa009fea4f03c616e9860e89ec725a7d6c9f
Mykyta Rakhmanyi <nikita.rahmany@gmail.com>
2026-02-03 15:13:51 +0100

    Add connection string from setting

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,12 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import DeclarativeBase
+from .config import setting
 
-engine = create_engine("sqlite:///data.db")
+engine = create_engine(setting.SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 
 class Base(DeclarativeBase):
     def __init__(self, **entries):
-        # ignore extra keywords when creating an instance
         self.__dict__.update(entries)
